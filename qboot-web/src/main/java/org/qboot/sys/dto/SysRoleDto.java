184354e9700b0d9ebc520d1cb1456242bfaa8090
iscast <376384272@qq.com>
2020-12-09 10:36:29 +0800

    角色模块不输出多余字段

--- qboot-web/src/main/java/org/qboot/sys/dto/SysRoleDto.java
+++ qboot-web/src/main/java/org/qboot/sys/dto/SysRoleDto.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import org.hibernate.validator.constraints.Length;
 
 import org.qboot.common.entity.BaseEntity;
@@ -11,15 +12,11 @@ import org.qboot.common.entity.BaseEntity;
 /**
  * <p>Title: SysRole</p>
  * <p>Description: 系统角色实体</p>
- * 
  * @author history
  * @date 2018-08-08
  */
 public class SysRoleDto extends BaseEntity<String>{
 
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	/**
@@ -30,17 +27,17 @@ public class SysRoleDto extends BaseEntity<String>{
     private String name;
 	
 	/**下列字段为业务字典**/
-	
+    @JsonInclude(JsonInclude.Include.NON_NULL)
 	private List<String> menuIds;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
 	private List<String> deptIds;
-	
+    @JsonInclude(JsonInclude.Include.NON_NULL)
 	private String authMenuIds;
 	
     public String getName() {
         return name;
     }
 
-    
     public void setName(String name) {
         this.name = name == null ? null : name.trim();
     }
@@ -49,26 +46,21 @@ public class SysRoleDto extends BaseEntity<String>{
 		return menuIds;
 	}
 
-
 	public void setMenuIds(List<String> menuIds) {
 		this.menuIds = menuIds;
 	}
-
 
 	public List<String> getDeptIds() {
 		return deptIds;
 	}
 
-
 	public void setDeptIds(List<String> deptIds) {
 		this.deptIds = deptIds;
 	}
 
-
 	public String getAuthMenuIds() {
 		return authMenuIds;
 	}
-
 
 	public void setAuthMenuIds(String authMenuIds) {
 		this.authMenuIds = authMenuIds;
